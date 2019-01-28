d7c6247cb601187bf7a4e84c699feef35a7b02e9
kennythepark <parkubc@gmail.com>
2019-01-28 04:20:26 -0800

    Update site for FW2019 preparation

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
+import logo from './main_logo.gif';
+import bg_img from './bg_image.jpg';
 import './App.css';
 
 class App extends Component {
@@ -7,18 +8,11 @@ class App extends Component {
     return (
       <div className="App">
         <header className="App-header">
+          <img src={bg_img} className="App-background" alt="bg_image" />
           <img src={logo} className="App-logo" alt="logo" />
           <p>
-            Edit <code>src/App.js</code> and save to reload.
+            FW 2019 | coming soon
           </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
         </header>
       </div>
     );
